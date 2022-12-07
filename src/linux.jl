1b30416109c44d5b839f349e4216168e80c5af36
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-12-07 11:36:43 +0600

    fool around with linux

--- src/linux.jl
+++ src/linux.jl
@@ -2,6 +2,6 @@ using FileIO
 
 function screenshot()
     file = tempname() * ".png"
-    save(file, load(pipeline(`xwd -root`, `convert xwd:- png:-`)))
+    save(file, load(read(pipeline(`xwd -root`, `convert xwd:- png:-`))))
     file
 end
